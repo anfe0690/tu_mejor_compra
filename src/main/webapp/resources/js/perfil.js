4a286856abf0a59ef49fc2c5b6dedf145c199223
Andres Felipe <anfe0690@gmail.com>
2013-12-18 13:05:56 -0500

    JPA - Manejo en javascript de eliminacion de productos

--- src/main/webapp/resources/js/perfil.js
+++ src/main/webapp/resources/js/perfil.js
@@ -1,16 +1,35 @@
 
 window.onload = function() {
 
-	var check_box_sel01 = document.getElementById("section_mis_productos_form:section_mis_productos_sel_01");
-	check_box_sel01.onclick = function (){
-		console.log("onclick");
-		var button_eliminar = document.getElementById("section_mis_productos_form:boton_eliminar");
-		if (button_eliminar.hasAttribute("disabled")){
-			button_eliminar.removeAttribute("disabled");
-		}else{
-			button_eliminar.setAttribute("disabled","disabled");
+	// Eliminar productos
+	var button_eliminar = document.getElementById("section_mis_productos_form:boton_eliminar");
+	var numFilas = document.getElementById("section_mis_productos_form:datatable1").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
+
+	var sels = new Array();
+	for (i = 0; i < numFilas; i++) {
+		sels[i] = document.getElementById("section_mis_productos_form:datatable1:" + i + ":seleccion");
+		sels[i].onclick = function() {
+			if (verificarSiAlgunInputSeleccionado()) {
+				button_eliminar.removeAttribute("disabled");
+			} else {
+				button_eliminar.setAttribute("disabled", "disabled");
+			}
+		};
+	}
+
+	function verificarSiAlgunInputSeleccionado() {
+		for (i = 0; i < sels.length; i++) {
+			if(sels[i].checked){
+				return true;
+			}
 		}
-	};
+		return false;
+	}
 	
+	document.getElementById("section_mis_productos_form").onsubmit = function (){
+		return confirm("¿Estas seguro de que deseas eliminar el/los producto(s) seleccionado(s)?");
+	};
 };
+
+
 
