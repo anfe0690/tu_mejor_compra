04c452e5efedea9b4870d70a4a275693e5a28505
Andres Felipe <anfe0690@gmail.com>
2013-12-24 11:28:56 -0500

    Agregada previsualizacion de la imagen que se utilizara para el producto por crear, utilizando javascript

--- src/main/webapp/resources/js/crear_producto.js
+++ src/main/webapp/resources/js/crear_producto.js
@@ -8,6 +8,18 @@ window.onload = function() {
 	
 	var inputFile = document.getElementById("form_crear_producto:file");
 	inputFile.setAttribute("required","required");
+	inputFile.setAttribute("accept","image/*");
+	inputFile.onchange = function (){
+		if(this.files && this.files[0]){
+			var reader = new FileReader();
+			
+			reader.onload = function (e){
+				document.getElementById("form_crear_producto:imagen").setAttribute("src",e.target.result);
+			};
+			
+			reader.readAsDataURL(this.files[0]);
+		}
+	};
 	
 	var inputNombre = document.getElementById("form_crear_producto:nombre_producto");
 	inputNombre.setAttribute("required","required");
