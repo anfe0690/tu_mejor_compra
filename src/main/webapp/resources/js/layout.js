d52177855e966d6036dd65086938bf2525d4ed4d
Andres Felipe <anfe0690@gmail.com>
2014-02-23 10:23:26 -0500

    Boton "Contraseñas", extraido javascript y ocultado cuando se inicia sesion

--- src/main/webapp/resources/js/layout.js
+++ src/main/webapp/resources/js/layout.js
@@ -1,31 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 window.onload = function() {
-	
-	/*
-	var btnAyuda = document.getElementById("ayuda");
-	btnAyuda.onclick = function (){
-		alert("Los usuarios y sus respectivas contraseñas son los siguientes:\n\n" +
-				"andres = '123'\n" +
-				"carlos = '456'\n" +
-				"fernando = '789'\n"
-				);
+
+	var btnAyuda = document.getElementById("contrasenas");
+	btnAyuda.onclick = function() {
+		alert("Los usuarios y sus respectivas contraseñas son los siguientes:\n\n"
+				+ "andres = '123'\n"
+				+ "carlos = '456'\n"
+				+ "fernando = '789'\n");
 	};
-	*/
-	
-	var inputBuscar = document.getElementById("header_form:texto_buscar");
-	inputBuscar.setAttribute("required","required");
-	
-	var form = document.getElementById("header_form");
-	form.onsubmit = function (e){
-		if(inputBuscar.value.trim().length===0){
+
+	var btnRestaurarBD = document.getElementById("restaurar:boton");
+	btnRestaurarBD.onclick = function(e) {
+		var r = confirm("Al restaurar la base de datos se perderan toads las modificaciones realizadas.\n¿Desea continuar?");
+		if (r == false) {
 			e.preventDefault();
 		}
-		
 	};
-	
+
+	var inputBuscar = document.getElementById("header_form:texto_buscar");
+	inputBuscar.setAttribute("required", "required");
+
+	var form = document.getElementById("header_form");
+	form.onsubmit = function(e) {
+		if (inputBuscar.value.trim().length === 0) {
+			e.preventDefault();
+		}
+
+	};
+
 };
