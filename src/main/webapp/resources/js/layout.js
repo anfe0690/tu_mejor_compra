d9c672a33d0d9bfeb5cc79a913ea6f1896deb790
Andres Felipe <anfe0690@gmail.com>
2014-04-24 17:45:30 -0500

    Creado inicio de sesion con componentes especificos de escritorio.

--- src/main/webapp/resources/js/layout.js
+++ src/main/webapp/resources/js/layout.js
@@ -1,15 +1,4 @@
 
 $(document).ready(function() {
 
-	var btnContrasenas = $("#form-sesion\\:contrasenas")[0];
-	if (btnContrasenas !== undefined) {
-		$("#form-sesion\\:contrasenas")[0].onclick = null;
-		btnContrasenas.onclick = function() {
-			alert("Los usuarios y sus respectivas contraseñas son los siguientes:\n\n"
-					+ "andres = '123'\n"
-					+ "carlos = '456'\n"
-					+ "fernando = '789'\n");
-		};
-	}
-
 });
