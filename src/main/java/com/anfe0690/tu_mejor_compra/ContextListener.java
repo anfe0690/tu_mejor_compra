38f7c48b1f23d9f0046322272533c79ffaf08187
Andres Felipe <anfe0690@gmail.com>
2014-04-28 19:24:48 -0500

    Mejorado campo de busqueda con autocompletado.

--- src/main/java/com/anfe0690/tu_mejor_compra/ContextListener.java
+++ src/main/java/com/anfe0690/tu_mejor_compra/ContextListener.java
@@ -10,29 +10,29 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class ContextListener implements ServletContextListener {
 
-	private static final Logger logger = LoggerFactory.getLogger(ContextListener.class);
+    private static final Logger logger = LoggerFactory.getLogger(ContextListener.class);
 
-	// Keys
-	public static final String K_DIR_DATOS = "K_DIR_DATOS";
-	public static final String K_SERVIDOR = "K_SERVIDOR";
+    // Keys
+    public static final String K_DIR_DATOS = "K_DIR_DATOS";
+    public static final String K_SERVIDOR = "K_SERVIDOR";
     public static final String K_HOST = "K_HOST";
     public static final String K_PUERTO_WEBSOCKETS = "K_PUERTO_WEBSOCKETS";
-	// Values
-	public static final String V_SERVIDOR_GLASSFISH = "V_SERVIDOR_GLASSFISH";
+    // Values
+    public static final String V_SERVIDOR_GLASSFISH = "V_SERVIDOR_GLASSFISH";
     public static final String V_SERVIDOR_WILDFLY_LOCAL = "V_SERVIDOR_WILDFLY_LOCAL";
     public static final String V_SERVIDOR_WILDFLY_REMOTO = "V_SERVIDOR_WILDFLY_REMOTO";
 
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
-		logger.info("contextInitialized");
-		if (System.getenv("OPENSHIFT_DATA_DIR") != null) {
-			System.setProperty(K_DIR_DATOS, System.getenv("OPENSHIFT_DATA_DIR"));
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("contextInitialized");
+        if (System.getenv("OPENSHIFT_DATA_DIR") != null) {
+            System.setProperty(K_DIR_DATOS, System.getenv("OPENSHIFT_DATA_DIR"));
             System.setProperty(K_SERVIDOR, V_SERVIDOR_WILDFLY_REMOTO);
-		} else {
-			System.setProperty(K_DIR_DATOS, "C:\\Users\\Andres\\Downloads\\test\\TuMejorCompra\\img\\");
+        } else {
+            System.setProperty(K_DIR_DATOS, "C:\\Users\\Andres\\Downloads\\test\\TuMejorCompra\\img\\");
             System.setProperty(K_SERVIDOR, V_SERVIDOR_WILDFLY_LOCAL);
-		}
-		logger.info("{} = {}", K_SERVIDOR, System.getProperty(K_SERVIDOR));
+        }
+        logger.info("{} = {}", K_SERVIDOR, System.getProperty(K_SERVIDOR));
 
         switch (System.getProperty(K_SERVIDOR)) {
             case V_SERVIDOR_WILDFLY_LOCAL: {
@@ -49,11 +49,10 @@ public class ContextListener implements ServletContextListener {
                 logger.error("Servidor desconocido! \"{}\"", System.getProperty(K_SERVIDOR));
             }
         }
+    }
 
-	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		logger.info("contextDestroyed");
-	}
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("contextDestroyed");
+    }
 }
--- src/main/java/com/anfe0690/tu_mejor_compra/managedbeans/HeaderBean.java
+++ src/main/java/com/anfe0690/tu_mejor_compra/managedbeans/HeaderBean.java
@@ -1,38 +1,55 @@
 package com.anfe0690.tu_mejor_compra.managedbeans;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Named
 @RequestScoped
 public class HeaderBean {
 
-	// Logger
-	private static final Logger logger = LoggerFactory.getLogger(HeaderBean.class);
-	//
-	private String valor;
+    // Logger
+    private static final Logger logger = LoggerFactory.getLogger(HeaderBean.class);
+    //
+    private static final List<String> palabrasMasUsadas = Arrays.asList("Ram","Tablet","Android","Samsung","Lte","Nintendo");
+    //
+    private String valor;
 
-	@PostConstruct
-	public void postConstruct() {
-		logger.trace("postConstruct");
-	}
+    @PostConstruct
+    public void postConstruct() {
+        logger.trace("postConstruct");
+    }
 
-	@PreDestroy
-	public void preDestroy() {
-		logger.trace("preDestroy");
-	}
+    @PreDestroy
+    public void preDestroy() {
+        logger.trace("preDestroy");
+    }
 
-	// Getters and setters
-	public String getValor() {
-		return valor;
-	}
+    public List<String> completar(String s) {
+        logger.trace("completar \"{}\"", s);
+        List<String> rs = new ArrayList<>();
+        for(String smu:palabrasMasUsadas){
+            if(smu.contains(s)){
+                rs.add(smu);
+            }
+        }
+        return rs;
+    }
 
-	public void setValor(String valor) {
-		this.valor = valor;
-	}
+    // Getters and setters
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
 
 }
