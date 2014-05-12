53170be51842abbf1aaab965ebdeae3215b68e2a
Andres Felipe <anfe0690@gmail.com>
2014-05-12 17:00:14 -0500

    perfil.xhtml - Agregado manejo de productos.

---
+++ src/main/webapp/resources/js/m/perfil.js
@@ -0,0 +1,64 @@
+var confirmacionParaEliminar = false;
+
+$(document).ready(function () {
+    $("title").html("Perfil - Tu mejor compra");
+
+    // Dialogo confirmacion
+    $("#btn-confirmar-eliminar-producto-no").click(function () {
+        $("#popupDialog").popup("close");
+    });
+    $("#btn-confirmar-eliminar-producto-si").click(function () {
+        $("#popupDialog").popup("close");
+        confirmacionParaEliminar = true;
+        $("#pagina-tmc\\:form-mis-productos\\:btn-eliminar-producto").trigger("click");
+    });
+
+    // Limpiar el cache en firefox
+    $(".producto").find("input[type='checkbox']").each(function () {
+        if ($(this).prop("checked") === true) {
+            $(this).trigger("click");
+        }
+    });
+
+    efectoSeleccionCheckBox();
+});
+
+function efectoSeleccionCheckBox() {
+    var checkBoxes = $(".producto").find("input[type='checkbox']");
+
+    // Efecto de seleccion
+    checkBoxes.change(function () {
+        if ($(this).prop("checked") === true) {
+            $(this).parents(".producto").css("background-color", "#87BBE6");
+        } else {
+            $(this).parents(".producto").css("background-color", "white");
+        }
+    });
+}
+
+function checkBoxAjax(event) {
+    /*console.info("event.type = " + event.type);
+    console.info("event.source = " + event.source);
+    console.info("event.status = " + event.status);*/
+    if (event.status === "success") {
+        $("#pagina-tmc\\:form-mis-productos\\:btn-eliminar-producto").button("refresh");
+    }
+}
+
+function buttonSubmitAjax(event){
+    if (event.status === "success") {
+        $("#pagina-tmc\\:form-mis-productos\\:btn-eliminar-producto").button();
+        $(".producto").find("input[type='checkbox']").checkboxradio();
+        efectoSeleccionCheckBox();
+    }
+}
+
+function confirmarEliminar(){
+    if( !confirmacionParaEliminar ){
+        $("#popupDialog").popup("open");
+    }else{
+        confirmacionParaEliminar = false;
+        return true;
+    }
+    return false;
+}
